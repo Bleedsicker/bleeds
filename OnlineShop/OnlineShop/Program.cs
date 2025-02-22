dd336beec346a7fad23f1cb19802a9aa1d13e9ed
nikita.kostrov <nikita01010@gmail.com>
2025-02-22 14:26:57 +0300

    Задание 1

---
+++ OnlineShop/OnlineShop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineShop;
+
+public class Shop
+{
+    public string User;
+    public string Password;
+
+    public void NewUser()
+    {
+        Console.WriteLine("1. register.\t2. login");
+        var loginText = Console.ReadLine();
+        var login = int.Parse(loginText);
+        string UserName;
+        string passwordUser;
+        if (login == 1)
+        {
+            Console.WriteLine("Register new user");
+            Console.WriteLine("Enter the user name");
+            UserName = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            passwordUser = Console.ReadLine();
+        }
+    }
+}
